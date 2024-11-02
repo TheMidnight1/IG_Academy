acbc19a662383830a6ad6bdc5a08b27b3f68fe48
Rikesh <karmacharyar4@gmail.com>
2024-11-02 18:04:10 +0545

    structure modified

---
+++ config/themeConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    CUSTOM_DETAIL_PAGE: {
+        "blog": 'page-blog',
+        // Add more key-value pairs as needed
+    },
+    
+    CUSTOM_LISTING_PAGE: {
+        "blog" : 'blog-lising',
+    }
+};
