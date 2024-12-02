36d21dd97fdb1eb91d1b5597c7e68ae2535506e8
TheMidnight1 <saugat.balami@gmail.com>
2024-12-02 15:07:57 +0545

    Call for tutor done

--- routes/themeRoutes.js
+++ routes/themeRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 const homeController = require("@/controllers/HomeController");
+if (!homeController) {
+    console.error("Error: HomeController is undefined. Check the module path or export.");
+    process.exit(1); // Exit the application to prevent runtime errors
+}
 
 
 // --------------FRONTEND ROUTES-------------------------------------
@@ -9,7 +13,10 @@ router.get("/", homeController.getPage);
 router.get("/page/:slug", homeController.getStaticPage);
 router.get("/category/:slug", homeController.getCategoryListingPage);
 router.get("/post/:postId", homeController.getPostDetailPage);
-router.get("/search/",homeController.getSearchPage);
+
+router.post('/submit-tutor-form', homeController.submitStudentForm);
+
+// router.get("/search/",homeController.getSearchPage);
 
 // router.get('/contact', (req, res) => {
 //     res.render('theme/IG_Academy/pages/contact',{ showingpage: 'contact'}); 
